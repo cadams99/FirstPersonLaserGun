2792b6595ec189455af92b4853d26ac36174c492
Claryse Adams <c_adams18@u.pacific.edu>
2021-02-02 18:36:27 -0800

    adding project to repo

---
+++ Claryse Adams_3DGame/Scripts/OrbScript.cs
@@ -0,0 +1,42 @@
+﻿/*************************************************************************
+ * AUTHOR: Claryse Adams
+ * DESCRIPTION: Script attached to PowerOrbs. Makes them
+ *  disappear when shot
+ ************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbScript : MonoBehaviour
+{
+    private bool shot; //was the orb shot?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shot = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void disappear()
+    {
+        if (shot == false) //has this cube been collected yet?
+        {//Increment the number of items that the player has collected a
+         //cube by calling the SetItemsCollected method in the PlayerScript
+            GetComponent<Renderer>().enabled = false; //Make the cube disappear
+            shot = true; //Update that the cube has been collected
+            GetComponent<AudioSource>().PlayOneShot(GetComponent<AudioSource>().clip); //play sound when shot
+        }
+
+    }
+
+    public bool IsShot()
+    {
+        return shot;
+    }
+}
---
+++ Claryse Adams_3DGame/Scripts/PlayerScript.cs
@@ -0,0 +1,64 @@
+﻿/*************************************************************************
+ * AUTHOR: Claryse Adams
+ * DESCRIPTION: Script attached to FPSController. Mainly used to
+ *  track how many orbs player has shot and maintain UI
+ ************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private int orbsShot; //number of orbs player has shot
+    public Text message; //track orbs shot
+    public float timeLeft = 120.0f; //gives player two minutes to collect orbs
+    public Text timerText; // used for showing time
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbsShot = 0; //Initialize orbs shot
+        message.text = "Orbs Left: 10"; //Initialize the Text
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape")) //press esc to quit game
+            Application.Quit();
+
+        message.text = "Orbs Left: " + (10 - orbsShot); //update number of orbs shot 
+
+        if(orbsShot >= 1) //start/update timer after first orb is shot
+            timeLeft -= Time.deltaTime;
+
+        //win condition
+        if (orbsShot == 10)
+        {
+            timerText.text = "You Win!";
+        }
+
+        //update timerText
+        else
+        {
+            timerText.text = "        " + (timeLeft).ToString("0");
+
+            //lose condition
+            if (timeLeft <= 0)
+            {
+                timerText.text = "You Lose!";
+            }
+        }
+    }
+
+    public int GetOrbsShot()
+    {
+        return orbsShot;
+    }
+
+    public void IncrementOrbs()
+    {
+        orbsShot++;
+    }
+}
